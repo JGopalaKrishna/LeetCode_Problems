2878b7888daf34407d99458c83f4518c5eb532dc
JGopalaKrishna <141515852+JGopalaKrishna@users.noreply.github.com>
2025-03-17 10:03:30 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (73.03%) - LeetSync

---
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.py
@@ -0,0 +1,7 @@
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        c=Counter(nums)
+        for i in c.values():
+            if i%2:
+                return False
+        return True
